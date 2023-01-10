8bf8af6b3812a53fda2adb5916fdc67c8bc27ba7
MacBook_Pro <macbook_pro@MacBook-Pro-MacBook-Pro.local>
2023-01-10 12:06:49 +0600

    first commit

---
+++ src/fire.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/app";
+import "firebase/compat/auth";
+const firebaseConfig = {
+    apiKey: "AIzaSyAs4Ii9vAm65qMJUG_KqFm7dwA1yEOioKg",
+    authDomain: "timur-ff3a4.firebaseapp.com",
+    projectId: "timur-ff3a4",
+    storageBucket: "timur-ff3a4.appspot.com",
+    messagingSenderId: "764465277640",
+    appId: "1:764465277640:web:fa3a1b4c925bc67f1113fd",
+};
+
+// Initialize Firebase
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import NavabarContexts from "./contexts/NavabarContext";
+import AuthContextProvider from "./contexts/AuthContextProvaider";
+import ProductContextProvider from "./contexts/ProductContextProvaider";
+import CartContextProvider from './contexts/CartContextProvaider'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthContextProvider>
+      <CartContextProvider>
+        <ProductContextProvider>
+          <AuthContextProvider>
+            <NavabarContexts>
+              <App />
+            </NavabarContexts>
+          </AuthContextProvider>
+        </ProductContextProvider>
+      </CartContextProvider>
+    </AuthContextProvider>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
